7e0061389a4553ace9ac524c641416c3e7b99cfe
Kashish Sahu <125540624+gitKashish@users.noreply.github.com>
2024-07-08 23:25:09 +0530

    Bug Fix - charset while parsing only from 'a' to 'x' instead of 'a' to 'z'

--- com/interpreter/ok/Scanner.java
+++ com/interpreter/ok/Scanner.java
@@ -129,7 +129,7 @@ public class Scanner {
     }
 
     private boolean isAlpha(char c) {
-        return (c >= 'a' && c <= 'x') ||
+        return (c >= 'a' && c <= 'z') ||
                 (c >= 'A' && c <= 'Z') ||
                 c == '_';
     }
